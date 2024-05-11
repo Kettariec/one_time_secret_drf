5177e22986a040af850095b2cca54683046e4a05
Kettariec <kettariec@gmail.com>
2024-05-11 11:55:36 +0300

    add test

--- keep_secrets/tests.py
+++ keep_secrets/tests.py
@@ -23,6 +23,27 @@ class SecretTestCase(APITestCase):
             status.HTTP_201_CREATED
         )
 
+    def test_error_create(self):
+        data = {
+            "text": 'test text',
+            "days": 8
+        }
+        response = self.client.post(
+            'http://127.0.0.1:8000/keep_secrets/create/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+        self.assertEqual(
+            response.json(),
+            {
+                'days': ['Ensure this value is less than or equal to 7.']
+            }
+        )
+
     def test_no_secret(self):
         response = self.client.get(
             'http://127.0.0.1:8000/keep_secrets/check/test/',
